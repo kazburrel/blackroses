41812b6b5f926f3482731206388349937ed8aa18
kazburrel <oegbosionu@gmail.com>
2024-04-14 14:08:35 +0100

    site details

--- database/migrations/2024_04_14_124409_create_settings_table.php
+++ database/migrations/2024_04_14_124409_create_settings_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('address')->nullable();
+            $table->string('postcode')->nullable();
             $table->string('phone')->nullable();
             $table->string('email')->nullable();
             $table->string('facebook')->nullable();
