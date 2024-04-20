b2b6d5126caf39199efb40e266c27826527ac46d
kazburrel <oegbosionu@gmail.com>
2024-04-20 15:08:39 +0100

    vaccancy post

--- resources/views/our-team.blade.php
+++ resources/views/our-team.blade.php
@@ -36,21 +36,44 @@
                             </figure>
                         </div>
                         <!--Content Column-->
-                        {{-- <div class="content-column">
+                        <div class="content-column">
                             <div class="inner">
                                 <h3>LATOYA HUGHES</h3>
                                 <p><b>Founder & CEO</b></p>
-                                <div class="text">Latoya Hughes, CEO of Black Roses Support, leverages 20+ years in social
-                                    care to empower youth. Her passion for aiding young people drove the establishment of
-                                    Black Roses Support, embodying resilience and compassion. Through her leadership, the
-                                    organization fosters positive change and nurtures the well-being of those it serves.
+                                <div class="text">Latoya Hughes, a distinguished Senior Social Worker, embodies a profound
+                                    commitment to uplifting marginalized youth. With a rich background in local authority
+                                    services, Latoya has honed her expertise in navigating the complexities of youth
+                                    welfare, particularly in areas fraught with challenges like serious youth violence,
+                                    child exploitation, and NEET circumstances.
+
+                                    As the driving force behind Black Roses Support, Latoya channels her passion into a
+                                    transformative vision: to provide a lifeline for young individuals grappling with
+                                    adversity. Drawing from her frontline experiences, she understands the nuanced needs of
+                                    the community and leverages this insight to tailor interventions that resonate deeply
+                                    with those they serve.
+
+                                    Under Latoya's leadership, Black Roses Support operates as more than just a service
+                                    provider; it's a beacon of hope and empowerment. Through a holistic approach that
+                                    integrates physical activities such as boxing and football with mentoring and skills
+                                    training, Latoya fosters an environment where youth not only find refuge but also
+                                    discover their innate potential to thrive.
+
+                                    Central to Black Roses Support's ethos is the belief in the inherent worth of every
+                                    individual. Latoya and her team go beyond surface-level interventions, delving into the
+                                    root causes of societal issues and offering tailored support that addresses the
+                                    multifaceted needs of their beneficiaries.
+
+                                    With a steadfast commitment to social justice and community well-being, Latoya Hughes
+                                    stands as a beacon of resilience and compassion in the field of social work. Through her
+                                    unwavering dedication and innovative approaches, she continues to make a profound
+                                    impact, one life at a time.
                                 </div>
                                 <div class="social-links">
                                     <a href="#"><span class="fa fa-facebook-f"></span></a>
                                     <a href="#"><span class="fa fa-linkedin"></span></a>
                                 </div>
                             </div>
-                        </div> --}}
+                        </div>
                     </div>
                 </div>
 
