94b71589a5af88f7119feb2e4f7521d99ef944f2
kazburrel <oegbosionu@gmail.com>
2024-04-21 19:24:41 +0100

    new

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Validator::extend('recaptcha', 'App\\Validators\\ReCaptcha@validate');
+        // Validator::extend('recaptcha', 'App\\Validators\\ReCaptcha@validate');
     }
 }
