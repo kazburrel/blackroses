905644abcf32ede3662904bb4bc236a29c72ced3
kazburrel <oegbosionu@gmail.com>
2024-04-25 18:39:00 +0100

    sitemap generator

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -10,11 +10,11 @@ class SitemapController extends Controller
 {
     public function __invoke()
     {
-        SitemapGenerator::create('https://blackrosessupport.co.uk/')
-            ->getSitemap()
-            ->add(Url::create('https://frimleyhealthandcare.org.uk/get-involved/innovation-fund/2022-east-berkshire-families-children-and-young-peoples-fund/black-roses-support/
-            ')->setPriority(0.5))
-            ->writeToFile('sitemap.xml');
+        SitemapGenerator::create('https://blackrosessupport.co.uk/')->writeToFile('sitemap.xml');
+        // ->getSitemap()
+        // ->add(Url::create('https://frimleyhealthandcare.org.uk/get-involved/innovation-fund/2022-east-berkshire-families-children-and-young-peoples-fund/black-roses-support/
+        // ')->setPriority(0.5))
+
 
         return 'site map created';
     }
