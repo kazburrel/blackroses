b6d6db5beb35d7eb477598698decaf62d8adaed0
kazburrel <oegbosionu@gmail.com>
2024-05-17 16:07:09 +0100

    Add team member

--- resources/views/livewire/team-members/team-members.blade.php
+++ resources/views/livewire/team-members/team-members.blade.php
@@ -54,8 +54,122 @@
                         </a>
                     </div>
                 </div>
+                <div class="modal fade" id="kt_modal_add_user-{{ $member->uuid }}" tabindex="-1" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered mw-650px">
+                        <div class="modal-content rounded">
+                            <div class="modal-header pb-0 border-0 justify-content-end">
+                                <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
+                                    <span class="svg-icon svg-icon-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none">
+                                            <rect opacity="0.5" x="6" y="17.3137" width="16" height="2"
+                                                rx="1" transform="rotate(-45 6 17.3137)" fill="currentColor" />
+                                            <rect x="7.41422" y="6" width="16" height="2" rx="1"
+                                                transform="rotate(45 7.41422 6)" fill="currentColor" />
+                                        </svg>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="modal-body scroll-y px-10 px-lg-15 pt-0 pb-15">
+                                <form wire:submit.prevent="addTeamMember">
+                                    @csrf
+                                    <div class="mb-13 text-center">
+                                        <h1 class="mb-3">Update Team Member</h1>
+                                    </div>
+                                    <div class="d-flex flex-column mb-8 fv-row">
+                                        <label class="d-flex align-items-center fs-6 fw-bold mb-2">
+                                            <span class="required">Full Name</span>
+                                        </label>
+                                        <input type="text" class="form-control form-control-solid"
+                                            placeholder="Enter Staff Fullname" wire:model="fullname"
+                                            value="{{ old('fullname', $member->fullname) }}" />
+                                        @error('fullname')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="row g-9 mb-8">
+                                        <div class="col-md-6 fv-row">
+                                            <label class="d-flex align-items-center fs-6 fw-bold mb-2">
+                                                <span class="required">Staff Image</span>
+                                            </label>
+                                            <input type="file" class="form-control form-control-solid"
+                                                placeholder="Enter Job image" wire:model="image"
+                                                value="{{ old('image') }}" />
+                                            @error('image')
+                                                <div class="alert alert-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <div class="col-md-6 fv-row">
+                                            <label class="d-flex align-items-center fs-6 fw-bold mb-2">
+                                                <span class="required">Staff Position</span>
+                                            </label>
+                                            <input type="text" class="form-control form-control-solid"
+                                                placeholder="Enter Staff Position" wire:model="position"
+                                                value="{{ old('position', $member->position) }}" />
+                                            @error('position')
+                                                <div class="alert alert-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-column mb-8">
+                                        <label class="fs-6 fw-bold mb-2">About Staff</label>
+                                        <textarea class="form-control form-control-solid" rows="3" wire:model="write_up"
+                                            placeholder="Please give a small write up about staff">{{ $member->write_up }}</textarea>
+                                        @error('write_up')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="reset" data-bs-dismiss="modal"
+                                            id="kt_modal_new_target_cancel" class="btn btn-light me-3">Cancel</button>
+                                        <button type="submit" id="kt_modal_new_target_submit"
+                                            class="btn btn-primary">
+                                            <span class="indicator-label">Submit</span>
+                                            <span class="indicator-progress">Please wait...
+                                                <span
+                                                    class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal fade" tabindex="-1" id="kt_modal_delete_job{{ $member->uuid }}">
+                    <div class="modal-dialog">
+                        <div class="modal-content text-center">
+                            <div class="modal-header">
+                                <h1 class="modal-title text-dark text-uppercase">Delete Job Listing</h1>
+                                <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal"
+                                    aria-label="Close">
+                                    <span class="svg-icon svg-icon-2x">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none">
+                                            <rect opacity="0.5" x="6" y="17.3137" width="16" height="2"
+                                                rx="1" transform="rotate(-45 6 17.3137)"
+                                                fill="currentColor" />
+                                            <rect x="7.41422" y="6" width="16" height="2" rx="1"
+                                                transform="rotate(45 7.41422 6)" fill="currentColor" />
+                                        </svg>
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div class="modal-body">
+                                <span class="text-danger">This action will also delete team member</span>
+                                <p>Are you sure you want to delete?</p>
+                            </div>
+
+                            <div class="modal-footer d-flex justify-content-center">
+                                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                                <x-delete href="/admin/all_courses/{{ $member->uuid }}" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @empty
             @endforelse
         </div>
     </div>
+
 </div>
