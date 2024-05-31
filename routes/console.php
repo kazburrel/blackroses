b4113fd269b24e634a972bed092a857a71a0c312
kazburrel <oegbosionu@gmail.com>
2024-05-31 13:44:26 +0100

    updated the validation rules for posting job application

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
 Schedule::command('send:cert-reminders')->everyMinute();
