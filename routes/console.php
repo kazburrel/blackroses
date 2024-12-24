8598ecef78ab1c50980a4e7fe74f1386b31609b8
kazburrel <oegbosionu@gmail.com>
2024-12-24 10:13:51 +0000

    Add daily birthday reminder command to console schedule

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Schedule;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 
 Schedule::command('send:cert-reminders')->everyMinute();
 
 Schedule::command('files:clear-unused')->everySixHours();
+
+Schedule::command('birthday:reminder')->dailyAt('8:00');
 
 // clear log
 // echo "" > storage/logs/laravel.log
